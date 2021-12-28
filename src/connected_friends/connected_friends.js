20677b21f053c35c106626fac6058b8ce3bb2b55
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2021-12-28 02:15:52 -0800

    queue and stack algorithms
    algorithms completed

--- src/connected_friends/connected_friends.js
+++ src/connected_friends/connected_friends.js
@@ -1,5 +1,36 @@
 const Queue = require("../lib/queue");
 
-const connected = (G, s, r) => {};
+//G = graph, s = startUser, r = endUser
+
+const connected = (G, s, r) => {
+
+    const users = Object.keys(G)
+    if (!users.length) return false;
+    if (s === r) return true;
+
+    const discovered = new Queue();
+
+    discovered.enqueue(s);
+    const enqueued = [s];
+
+    while (discovered.first) {
+        const user = discovered.dequeue();
+        const following = G[user];
+
+        for (const followedUser of following) {
+            if (followedUser === r) {
+                return true;
+            }
+        
+
+        if (!enqueued.includes(followedUser)) {
+            enqueued.push(followedUser);
+            discovered.enqueue(followedUser);
+        }
+    }
+    }
+
+    return false
+};
 
 module.exports = connected;
